ce6cfeed697176670871c528e574a0c21f27c66d
ammieprsd <ammieprsd@gmail.com>
2022-04-26 15:26:13 +0200

    POM en Properties checked

---
+++ src/main/java/nl/novi/springles1controller/SpringLes1ControllerApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.springles1controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLes1ControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLes1ControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/springles1controller/SpringLes1ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.springles1controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLes1ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
